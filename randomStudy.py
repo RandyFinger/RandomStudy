6617f72550ea9cb6ad1e2ef8b1ca998cfa3222d6
Randy Finger <randy93finger@gmail.com>
2021-01-01 16:58:18 -0500

    correcting logic in randint

--- randomStudy.py
+++ randomStudy.py
@@ -11,7 +11,7 @@ a = np.zeros((10), dtype=np.int16)
 # iterate through array and assign random values between 1 and 10
 i = 0
 while i < a.size:
-    a[i] = random.randint(1, 11)
+    a[i] = random.randint(1, 10)
     i += 1
 
 # Print array a
