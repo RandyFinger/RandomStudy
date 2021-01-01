6e3a3ec9fa83b59777e7ff41cd5a937f819a0dbc
Randy Finger <randy93finger@gmail.com>
2021-01-01 16:55:13 -0500

    Generate random numpy array

--- randomStudy.py
+++ randomStudy.py
@@ -1,2 +1,18 @@
+# This is an ongoing study to research the behavior of  python's native random library. 
+
 import pandas as pd
 import numpy as np
+import matplotlib as plot
+import random
+
+# Generate an array of random values. 1d array size = 10 
+a = np.zeros((10), dtype=np.int16)
+
+# iterate through array and assign random values between 1 and 10
+i = 0
+while i < a.size:
+    a[i] = random.randint(1, 11)
+    i += 1
+
+# Print array a
+print(a)
